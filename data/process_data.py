01461a8d5f71746307d839a09d273b392b50e0a9
Hans Kristian <hanskristian0111@gmail.com>
2021-07-13 00:08:11 +0700

    Perform ETL Pipelines on the dataset

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,46 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Load messages and categories from csv
+    '''
+    messages = pd.read_csv(messages_filepath, index_col=0)
+    categories = pd.read_csv(categories_filepath, index_col=0)
+    df = messages.merge(categories, left_index=True, right_index=True)
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    Clean the categories column and drop duplicates
+    '''
+    categories = df['categories'].str.split(';', expand=True)  # Create DataFrame of 36 individual category columns
+    category_colnames = categories.loc[2, :].str.split('-', expand=True)[0]
+    categories.columns = category_colnames  # rename the columns of `categories`
+
+    for column in categories:
+        # set each value to be the last character of the string and convert to numeric
+        categories[column] = categories[column].str.split('-', expand=True)[1].astype(int)
+
+    categories.replace({2: 1}, inplace=True)
+
+    df = df.drop(['categories', 'original'], axis=1)
+    df = pd.concat([df, categories], axis=1)
+
+    duplicates = df['message'].duplicated()
+    # drop duplicates
+    df = df[~duplicates]
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    Export the cleaned data to sqlite database
+    '''
+    engine = create_engine(f'sqlite:///{database_filename}')
+    df.to_sql('disaster_response', engine, index=False, if_exists='replace')
 
 
 def main():
