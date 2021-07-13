56c4671944e282c667090d62a9a4a162a0e05063
Hans Kristian <hanskristian0111@gmail.com>
2021-07-13 18:37:21 +0700

    Create ML model to classify messages

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,89 @@
 import sys
+import nltk
+import numpy
+import pandas as pd
+import joblib
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.metrics import confusion_matrix, classification_report, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.decomposition import TruncatedSVD
 
+nltk.download(['stopwords', 'wordnet', 'punkt'], quiet=True)
 
 def load_data(database_filepath):
-    pass
+    '''
+    Load Predictor, Response, and Category Names from sqlite database
+    '''
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql('SELECT * FROM disaster_response', engine)
+
+    category_names = list(df.columns[2:])
+    X = df['message'].values
+    Y = df.drop(['message', 'genre'], axis=1).values
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    '''
+    Processes text to list of tokens
+    
+    INPUT:
+    text
+    
+    OUTPUT:
+    list - tokens generated from the text
+    '''
+    
+    # normalize and tokenize text
+    tokens = word_tokenize(text.lower())
+    
+    # lemmatize and remove stopwords
+    tokens = [WordNetLemmatizer().lemmatize(word) for word in tokens if word not in stopwords.words('english')]
+    
+    return tokens
 
 
 def build_model():
-    pass
+    '''
+    Build Machine Learning Pipeline Model and Use Grid Search to find the optimal parameters
+    '''
+    pipeline = Pipeline([('vect', TfidfVectorizer(tokenizer=tokenize)),
+        ('dim_red', TruncatedSVD(350)),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_jobs=-1,
+            class_weight='balanced')))])
+
+    parameters = {'dim_red__n_components': [150, 250, 350],
+        'clf__estimator__min_samples_leaf': [75, 100, 150]}
+
+    model = GridSearchCV(pipeline, parameters)
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Evaluate model on test data
+    '''
+    Y_pred = model.predict(X_test)
+    for i in range(Y_test.shape[1]):
+        print(category_names[i])
+        print(classification_report(Y_test[:, i], Y_pred[:, i], zero_division=0))
+    
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Save the model as pickle file
+    '''
+    joblib.dump(model, model_filepath, compress=3)
+    
 
 
 def main():
@@ -26,7 +91,7 @@ def main():
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
         
         print('Building model...')
         model = build_model()
@@ -38,7 +103,7 @@ def main():
         evaluate_model(model, X_test, Y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        save_model(model.best_estimator_, model_filepath)
 
         print('Trained model saved!')
 
