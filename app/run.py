c779b7453354921ef440879cbde8cd976ed5aa4a
Hans Kristian <hanskristian0111@gmail.com>
2021-07-13 18:44:22 +0700

    Add changes to run the web app

--- app/run.py
+++ app/run.py
@@ -1,36 +1,45 @@
 import json
 import plotly
 import pandas as pd
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
+import joblib
+import nltk
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
+nltk.download(['stopwords', 'wordnet', 'punkt'], quiet=True)
 
 app = Flask(__name__)
 
 def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
+    '''
+    Processes text to list of tokens
+    
+    INPUT:
+    text
+    
+    OUTPUT:
+    list - tokens generated from the text
+    '''
+    
+    # normalize and tokenize text
+    tokens = word_tokenize(text.lower())
+    
+    # lemmatize and remove stopwords
+    tokens = [WordNetLemmatizer().lemmatize(word) for word in tokens if word not in stopwords.words('english')]
+    
+    return tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///data/DisasterResponse.db')
+df = pd.read_sql_table('disaster_response', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -40,7 +49,7 @@ def index():
     
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
+    genre_counts = df['genre'].value_counts()
     genre_names = list(genre_counts.index)
     
     # create visuals
@@ -82,7 +91,7 @@ def go():
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
+    classification_results = dict(zip(df.columns[2:], classification_labels))
 
     # This will render the go.html Please see that file. 
     return render_template(
